cedefc54c7ba0f56cbd4457dc6d99826ae3c5ffe
Matvey <veleskg@mail.ru>
2023-10-03 23:32:08 +0500

    Страница авторизации + профиль пользователя

--- web/src/components/Login.tsx
+++ web/src/components/Login.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../styles/tokenInputPage.css';
 
 interface LoginProps {
@@ -7,10 +8,34 @@ interface LoginProps {
 
 const Login: React.FC<LoginProps> = ({ setToken }) => {
     const [tokenInput, setTokenInput] = useState<string>('');
+    const navigate = useNavigate(); // Использование useNavigate для перенаправления
 
     const handleLogin = () => {
         setToken(tokenInput);
         localStorage.setItem('token', tokenInput);
+
+        const apiUrl = 'http://localhost:8080/get_user';
+
+        fetch(apiUrl, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${tokenInput}`
+            }
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('User data:', data);
+
+                navigate(`/profile?token=${tokenInput}`);
+            })
+            .catch(error => {
+                console.error('Error fetching user data:', error);
+            });
     };
 
     return (
@@ -22,7 +47,6 @@ const Login: React.FC<LoginProps> = ({ setToken }) => {
                 value={tokenInput}
                 onChange={(e) => setTokenInput(e.target.value)}
                 style={{ color: 'black' }}
-
             />
             <button onClick={handleLogin} style={{ backgroundColor: 'white', color: 'black' }}>
                 Войти
