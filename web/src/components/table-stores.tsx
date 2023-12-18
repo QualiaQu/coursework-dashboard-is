8fa4f50ac2dc6b9b32b2ecb8e8b1050b661a9f83
Vilena <vilena.saitgalina@mail.ru>
2023-12-19 00:27:27 +0500

    сохранение работает*

--- web/src/components/table-stores.tsx
+++ web/src/components/table-stores.tsx
@@ -25,6 +25,40 @@ export function TableStores({
                                 version,
                                 stores,
                               }: DataTableProps){
+    const [versionN, setVersionN] = useState(version)
+    const [storesN, setStoresN] = useState<Store[]>(stores)
+    const [storeGoogle, setGoogle] = useState<Store>()
+    const [storeApple, setApple] = useState<Store>()
+    const [storeHuawei, setHuawei] = useState<Store>()
+    const [GoogleDeplotInfo, setGoogleDeplotInfo] = useState("Введите даты")
+
+    useEffect(() => {
+        async function initialize() {
+            setStoresN(stores)
+            if(stores.length == 0){
+                return
+            }else if(stores.length == 1){
+                setGoogle(stores[0])
+            }else if(stores.length == 2){
+                setApple(stores[1])
+            }
+        }
+        initialize();
+    }, [stores]);
+
+    useEffect(() => {
+        async function initialize() {
+            if(storesN.length == 0){
+                return
+            }else if(storesN.length > 0){
+                setGoogle(storesN[0])
+                setApple(storesN[1])
+                setHuawei(storesN[2])
+                console.log("apple useeffect", storesN[1])
+            }
+        }
+        initialize();
+    }, [storesN]);
     // const version = JSON.parse(localStorage.getItem('versionInfo')) //= localStorage.getItem('versionInfo');
     // const magaz = localStorage.getItem('storesInfo') != null ? JSON.parse(localStorage.getItem('storesInfo')) : []
     // const [storesi, setStores] = useState<Store[]>([])
@@ -48,54 +82,109 @@ export function TableStores({
     //     return data;
     // }
 
-    const handleSave = () => {
+    const handleSaveGoogle = () => {
         // console.log('inf:', inf)
-        const appleDeployInfo = localStorage.getItem('aplleDeployInfo').replace('"', "")
-        const applePercentageInfo = localStorage.getItem('apllePercentageInfo').replace('"', "")
-        const appleErrorsInfo = localStorage.getItem('aplleErrorsInfo').replace('"', "")
-        const googleDeployInfo = localStorage.getItem('googleDeployInfo').replace('"', "")
-        const googlePercentageInfo = localStorage.getItem('googlePercentageInfo').replace('"', "")
-        const googleErrorsInfo = localStorage.getItem('googleErrorsInfo').replace('"', "")
+        // const appleDeployInfo = localStorage.getItem('aplleDeployInfo').replace('"', "")
+        // const applePercentageInfo = localStorage.getItem('apllePercentageInfo').replace('"', "")
+        // const appleErrorsInfo = localStorage.getItem('aplleErrorsInfo').replace('"', "")
+        // const googleDeployInfo = localStorage.getItem('googleDeployInfo').replace('"', "")
+        // const googlePercentageInfo = localStorage.getItem('googlePercentageInfo').replace('"', "")
+        // const googleErrorsInfo = localStorage.getItem('googleErrorsInfo').replace('"', "")
         if(version != '' || version != null || version != 'null'){
-            const apiUrlApple = `http://localhost:8080/set_version_info?version=${version}&store=Apple&deployDate=${appleDeployInfo}&installPercentage=${applePercentageInfo}&isErrors=${appleErrorsInfo}`;
-            console.log('url:', apiUrlApple)
-            const apiUrlGoogle = `http://localhost:8080/set_version_info?version=${version}&store=Google%20Play&deployDate=${googleDeployInfo}&installPercentage=${googlePercentageInfo}&isErrors=${googleErrorsInfo}`;
-            console.log('url:', apiUrlGoogle)
 
-            axios.post(apiUrlApple)
+            const apiUrlGoogle = `http://localhost:8080/set_version_info?version=${version}&store=Google%20Play&deployDate=${storeGoogle?.DeployDate}&installPercentage=${storeGoogle?.InstallPercentage}&isErrors=${storeGoogle?.IsErrors}`;
+            console.log('url:', apiUrlGoogle)
+            axios.post(apiUrlGoogle)
                 .catch(error => {
                     console.error('Ошибка в пост запросе у магазинов:', error);
                 });
-            axios.post(apiUrlGoogle)
+
+
+            // Promise.all([
+            //     fetch(`http://localhost:8080/set_version_info?version=${version}&store=Google%20Play&deployDate=${storeGoogle?.DeployDate}&installPercentage=${storeGoogle?.InstallPercentage}&isErrors=${storeGoogle?.IsErrors}`),
+            //     fetch(`http://localhost:8080/set_version_info?version=${version}&store=Apple&deployDate=${storeApple?.DeployDate}&installPercentage=${storeApple?.InstallPercentage}&isErrors=${storeApple?.IsErrors}`)
+            // ])
+            //     .then(async([res1, res2]) => {
+            //         const a = await res1.json();
+            //         const b = await res2.json();
+            //         console.log(a, 'AAA public repos on GitHub');
+            //         console.log(b, 'BBB public repos on GitHub');
+            //     })
+            //     .catch(error => {
+            //         console.log(error);
+            //     });
+
+        //     axios.all([
+        //         axios.post(`http://localhost:8080/set_version_info?version=${version}&store=Google%20Play&deployDate=${storeGoogle?.DeployDate}&installPercentage=${storeGoogle?.InstallPercentage}&isErrors=${storeGoogle?.IsErrors}`),
+        //         axios.post(`http://localhost:8080/set_version_info?version=${version}&store=Apple&deployDate=${storeApple?.DeployDate}&installPercentage=${storeApple?.InstallPercentage}&isErrors=${storeApple?.IsErrors}`)
+        //     ])
+        //         .then(axios.spread((data1, data2) => {
+        //             // output of req.
+        //             console.log('data1', data1, 'data2', data2)
+        //         }));
+        }
+
+    };
+    const handleSaveApple = () => {
+        if(version != '' || version != null || version != 'null'){
+
+            const apiUrlApple = `http://localhost:8080/set_version_info?version=${version}&store=Apple&deployDate=${storeApple?.DeployDate}&installPercentage=${storeApple?.InstallPercentage}&isErrors=${storeApple?.IsErrors}`;
+            console.log('url:', apiUrlApple)
+            axios.post(apiUrlApple)
                 .catch(error => {
                     console.error('Ошибка в пост запросе у магазинов:', error);
                 });
         }
 
     };
-    function handleChangeAplleDeplotInfo(event) {
-        localStorage.setItem('aplleDeployInfo', event.target.value);
+    const handleSaveHuawei = () => {
+        if(version != '' || version != null || version != 'null'){
+
+            const apiUrlHuawei = `http://localhost:8080/set_version_info?version=${version}&store=Huawei&deployDate=${storeHuawei?.DeployDate}&installPercentage=${storeHuawei?.InstallPercentage}&isErrors=${storeHuawei?.IsErrors}`;
+            console.log('url:', apiUrlHuawei)
+            axios.post(apiUrlHuawei)
+                .catch(error => {
+                    console.error('Ошибка в пост запросе у магазинов:', error);
+                });
+        }
+
+    };
+    const ChangeGoogleDeplotInfo = (event) => {
         console.log(event.target.value);
-    }
-    function handleChangeApllePercentageInfo(event) {
-        localStorage.setItem('apllePercentageInfo', event.target.value);
-        console.log(event.target.value);
-    }
-    function handleChangeAplleErrorsInfo(event) {
-        localStorage.setItem('aplleErrorsInfo', event.target.value);
-        console.log(event.target.value);
-    }
-    function handleChangeGoogleDeplotInfo(event) {
-        localStorage.setItem('googleDeployInfo', event.target.value);
-        console.log(event.target.value);
-    }
-    function handleChangeGooglePercentageInfo(event) {
-        localStorage.setItem('googlePercentageInfo', event.target.value);
-        console.log(event.target.value);
-    }
-    function handleChangeGoogleErrorsInfo(event) {
-        localStorage.setItem('googleErrorsInfo', event.target.value);
-        console.log(event.target.value);
+        console.log("id", event.target.id);
+
+        // loop over the todos list and find the provided id.
+        let updatedList = storesN.map(item =>
+        {
+            if(item.Store == "Google Play") {
+                if (event.target.id == 'google-deploy') {
+                    return {...item, DeployDate: event.target.value}; //gets everything that was already in item, and updates "done"
+                } else if (event.target.id == 'google-percentage') {
+                    return {...item, InstallPercentage: event.target.value}; //gets everything that was already in item, and updates "done"
+                } else if (event.target.id == 'google-errors') {
+                    return {...item, IsErrors: event.target.value};//gets everything that was already in item, and updates "done"
+                }
+            }else if(item.Store == "Apple") {
+                if (event.target.id == 'apple-deploy') {
+                    return {...item, DeployDate: event.target.value}; //gets everything that was already in item, and updates "done"
+                } else if (event.target.id == 'apple-percentage') {
+                    return {...item, InstallPercentage: event.target.value}; //gets everything that was already in item, and updates "done"
+                } else if (event.target.id == 'apple-errors') {
+                    return {...item, IsErrors: event.target.value}; //gets everything that was already in item, and updates "done"
+                }
+            }else if(item.Store == "Huawei") {
+                if (event.target.id == 'huawei-deploy') {
+                    return {...item, DeployDate: event.target.value}; //gets everything that was already in item, and updates "done"
+                } else if (event.target.id == 'huawei-percentage') {
+                    return {...item, InstallPercentage: event.target.value}; //gets everything that was already in item, and updates "done"
+                } else if (event.target.id == 'huawei-errors') {
+                    return {...item, IsErrors: event.target.value}; //gets everything that was already in item, and updates "done"
+                }
+            }
+
+            return item; // else return unmodified item
+        });
+        setStoresN(updatedList); // set state to new object with updated list
     }
 
     // if (version == "" || magaz == 'null' || magaz == null || magaz == ""){
@@ -179,6 +268,7 @@ export function TableStores({
 
     // const sInfo = localStorage.getItem('infoStores') != null ? JSON.parse(localStorage.getItem('infoStores')) : []
     console.log("infoPROPS", stores, stores.length, stores.length >= 1 )
+    console.log("infoUserState", storesN, storesN.length, storesN.length >= 1 )
     return(
         <div className="table-platforms-cont pt-2">
             <div className="table-platforms border rounded-md p-1 font-medium text-sm text-muted-foreground">
@@ -204,15 +294,45 @@ export function TableStores({
 
                 <div className="cell">
                     <h2 className="w-[110px] shop google">Google</h2>
-                    <Input className="w-[110px] cell-deploy" value={stores.length >= 1 ? stores[0].DeployDate : 'Введите дату'} onChange={handleChangeGoogleDeplotInfo}></Input>
-                    <Input className="w-[110px] cell-percentage" value={(stores.length >= 1) ? stores[0].InstallPercentage : 'Введите процент'}  onChange={handleChangeGooglePercentageInfo}></Input> {/*storeGoogle?.InstallPercentage*/}
-                    <Input className="w-[110px] cell-percentage" value={(stores.length >= 1) ? stores[0].IsErrors : 'Есть ли ошибки?'} onChange={handleChangeGoogleErrorsInfo}></Input>
+                    <Input id={"google-deploy"} className="w-[110px] cell-deploy" value={storesN.length >= 1 ? (storesN[0].DeployDate != "undefined" ? storesN[0].DeployDate : 'Введите дату') : 'Введите дату'} onChange={ChangeGoogleDeplotInfo}></Input>
+                    <Input id={"google-percentage"} className="w-[110px] cell-percentage" value={(storesN.length >= 1) ? (storesN[0].InstallPercentage != "undefined" ? storesN[0].InstallPercentage : 'Введите процент') : 'Введите процент'}  onChange={ChangeGoogleDeplotInfo}></Input> {/*storeGoogle?.InstallPercentage*/}
+                    <Input id={"google-errors"} className="w-[110px] cell-percentage" value={(storesN.length >= 1) ? (storesN[0].IsErrors /* == "0" ? "false" : "true"*/) : 'Есть ли ошибки?'} onChange={ChangeGoogleDeplotInfo}></Input>
+                    <Button
+                        variant="default"
+                        size="sm"
+                        className="-ml-3 h-8 data-[state=open]:bg-accent ml-0.5"
+                        onClick={handleSaveGoogle}
+                    >
+                        <span>{"Сохранить"}</span>
+                    </Button>
                 </div>
                 <div className="cell">
                     <h2 className="w-[110px] shop apple">Apple</h2>
-                    <Input className="w-[110px] cell-deploy" value={(stores.length >= 2) ? stores[1].DeployDate : 'Введите дату'} onChange={handleChangeAplleDeplotInfo}></Input>
-                    <Input className="w-[110px] cell-percentage" value={(stores.length >= 2) ? stores[1].InstallPercentage : 'Введите процент'} onChange={handleChangeApllePercentageInfo}></Input>
-                    <Input className="w-[110px] cell-percentage" value={(stores.length >= 2) ?  stores[1].IsErrors : 'Есть ли ошибки?'} onChange={handleChangeAplleErrorsInfo}></Input>
+                    <Input id={"apple-deploy"} className="w-[110px] cell-deploy" value={(storesN.length >= 2) ? (storesN[1].DeployDate != "undefined" ? storesN[1].DeployDate : 'Введите дату') : 'Введите дату'} onChange={ChangeGoogleDeplotInfo}></Input>
+                    <Input id={"apple-percentage"} className="w-[110px] cell-percentage" value={(storesN.length >= 2) ? (storesN[1].InstallPercentage != "undefined" ? storesN[1].InstallPercentage : 'Введите процент') : 'Введите процент'} onChange={ChangeGoogleDeplotInfo}></Input>
+                    <Input id={"apple-errors"} className="w-[110px] cell-percentage" value={(storesN.length >= 2) ?  storesN[1].IsErrors : 'Есть ли ошибки?'} onChange={ChangeGoogleDeplotInfo}></Input>
+                    <Button
+                        variant="default"
+                        size="sm"
+                        className="-ml-3 h-8 data-[state=open]:bg-accent ml-0.5"
+                        onClick={handleSaveApple}
+                    >
+                        <span>{"Сохранить"}</span>
+                    </Button>
+                </div>
+                <div className="cell">
+                    <h2 className="w-[110px] shop apple">Huawei</h2>
+                    <Input id={"huawei-deploy"} className="w-[110px] cell-deploy" value={(storesN.length >= 2) ? (storesN[2].DeployDate != "undefined" ? storesN[2].DeployDate : 'Введите дату') : 'Введите дату'} onChange={ChangeGoogleDeplotInfo}></Input>
+                    <Input id={"huawei-percentage"} className="w-[110px] cell-percentage" value={(storesN.length >= 2) ? (storesN[2].InstallPercentage != "undefined" ? storesN[2].InstallPercentage : 'Введите процент') : 'Введите процент'} onChange={ChangeGoogleDeplotInfo}></Input>
+                    <Input id={"huawei-errors"} className="w-[110px] cell-percentage" value={(storesN.length >= 2) ?  storesN[2].IsErrors : 'Есть ли ошибки?'} onChange={ChangeGoogleDeplotInfo}></Input>
+                    <Button
+                        variant="default"
+                        size="sm"
+                        className="-ml-3 h-8 data-[state=open]:bg-accent ml-0.5"
+                        onClick={handleSaveHuawei}
+                    >
+                        <span>{"Сохранить"}</span>
+                    </Button>
                 </div>
 
                 {/*<div className="cell">*/}
@@ -231,14 +351,14 @@ export function TableStores({
             {/*<button id="button-save" >*/}
             {/*    Сохранить*/}
             {/*</button>*/}
-                <Button
-                    variant="default"
-                    size="sm"
-                    className="-ml-3 h-8 data-[state=open]:bg-accent ml-0.5"
-                    onClick={handleSave}
-                >
-                    <span>{"Сохранить"}</span>
-                </Button>
+            {/*    <Button*/}
+            {/*        variant="default"*/}
+            {/*        size="sm"*/}
+            {/*        className="-ml-3 h-8 data-[state=open]:bg-accent ml-0.5"*/}
+            {/*        onClick={handleSave}*/}
+            {/*    >*/}
+            {/*        <span>{"Сохранить"}</span>*/}
+            {/*    </Button>*/}
             </div>
         </div>
     );
